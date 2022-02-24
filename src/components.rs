e57ef6901ffdc7df3a32221ba0d2c87f96792b4c
Mike Ask <mike.ask@defectdriven.com>
2022-02-24 13:47:33 -0600

    End fo chapter 9 - adding game over and victory screens

--- src/components.rs
+++ src/components.rs
@@ -16,6 +16,15 @@ pub struct Enemy;
 pub struct MovingRandomly;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ChasingPlayer;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Item;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AmuletOfYala;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
     pub entity : Entity,
     pub destination: Point,
@@ -35,3 +44,5 @@ pub struct Health {
 
 #[derive(Clone, PartialEq)]
 pub struct Name(pub String);
+
+
